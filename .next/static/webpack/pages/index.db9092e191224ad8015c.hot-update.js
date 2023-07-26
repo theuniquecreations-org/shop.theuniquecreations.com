6bd835a0411edf36532eb1318becf8c016335fc2
bala <vbalakumar.cse@gmail.com>
2023-07-26 00:52:40 -0400

    changes

---
+++ .next/static/webpack/pages/index.db9092e191224ad8015c.hot-update.js
@@ -0,0 +1,14 @@
+webpackHotUpdate_N_E("pages/index",{
+
+/***/ "./src/data/navigator.json":
+/*!*********************************!*\
+  !*** ./src/data/navigator.json ***!
+  \*********************************/
+/*! exports provided: HOME, SHOP, BLOG, PAGES, ABOUT, default */
+/***/ (function(module) {
+
+eval("module.exports = JSON.parse(\"{\\\"HOME\\\":{\\\"title\\\":\\\"Home\\\",\\\"href\\\":\\\"/\\\",\\\"subMenu\\\":[]},\\\"SHOP\\\":{\\\"title\\\":\\\"Shop\\\",\\\"href\\\":\\\"/shop/shop-grid-3-column\\\",\\\"subMenu\\\":{\\\"layout\\\":[{\\\"title\\\":\\\"Shop Fullwidth\\\",\\\"href\\\":\\\"/shop/shop-fullwidth\\\"},{\\\"title\\\":\\\"Shop 3 Column\\\",\\\"href\\\":\\\"/shop/shop-3-column\\\"}],\\\"detail\\\":[{\\\"title\\\":\\\"Shop Detail Fullwidth\\\",\\\"href\\\":\\\"/shop/product-detail-fullwidth\\\"},{\\\"title\\\":\\\"Shop Detail Sidebar\\\",\\\"href\\\":\\\"/shop/product-detail-sidebar\\\"}],\\\"pages\\\":[{\\\"title\\\":\\\"Checkout\\\",\\\"href\\\":\\\"/shop/checkout\\\"},{\\\"title\\\":\\\"Order Complete\\\",\\\"href\\\":\\\"/shop/order-complete\\\"},{\\\"title\\\":\\\"Wishlist\\\",\\\"href\\\":\\\"/shop/wishlist\\\"},{\\\"title\\\":\\\"Compare\\\",\\\"href\\\":\\\"/shop/compare\\\"},{\\\"title\\\":\\\"Cart\\\",\\\"href\\\":\\\"/shop/cart\\\"}]}},\\\"BLOG\\\":{\\\"title\\\":\\\"Blog\\\",\\\"href\\\":\\\"/blog/blog-list-sidebar\\\",\\\"subMenu\\\":[{\\\"title\\\":\\\"Blog List Sidebar\\\",\\\"href\\\":\\\"/blog/blog-list-sidebar\\\"},{\\\"title\\\":\\\"Blog Grid 2 Column\\\",\\\"href\\\":\\\"/blog/blog-grid-2-column\\\"},{\\\"title\\\":\\\"Blog Grid Sidebar\\\",\\\"href\\\":\\\"/blog/blog-grid-sidebar\\\"},{\\\"title\\\":\\\"Blog Masonry\\\",\\\"href\\\":\\\"/blog/blog-masonry\\\"},{\\\"title\\\":\\\"Blog Grid 1 Column Sidebar\\\",\\\"href\\\":\\\"/blog/blog-grid-1-column-sidebar\\\"},{\\\"title\\\":\\\"Blog Detail Sidebar\\\",\\\"href\\\":\\\"/blog/blog-detail\\\"}]},\\\"PAGES\\\":{\\\"title\\\":\\\"Pages\\\",\\\"href\\\":\\\"#\\\",\\\"subMenu\\\":[{\\\"title\\\":\\\"Login\\\",\\\"href\\\":\\\"/auth/login\\\"},{\\\"title\\\":\\\"Register\\\",\\\"href\\\":\\\"/auth/register\\\"},{\\\"title\\\":\\\"FAQ\\\",\\\"href\\\":\\\"/other/faq\\\"},{\\\"title\\\":\\\"Coming Soon\\\",\\\"href\\\":\\\"/other/coming-soon\\\"},{\\\"title\\\":\\\"Contact Us\\\",\\\"href\\\":\\\"/other/contact-us\\\"}]},\\\"ABOUT\\\":{\\\"title\\\":\\\"About us\\\",\\\"href\\\":\\\"/other/about-us\\\"}}\");//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IiIsImZpbGUiOiIuL3NyYy9kYXRhL25hdmlnYXRvci5qc29uLmpzIiwic291cmNlUm9vdCI6IiJ9\n//# sourceURL=webpack-internal:///./src/data/navigator.json\n");
+
+/***/ })
+
+})
--- src/components/header/elements/CategoryColappse.js
+++ src/components/header/elements/CategoryColappse.js
@@ -4,36 +4,21 @@ import { DownOutlined } from "@ant-design/icons";
 import Link from "next/link";
 
 let categories = [
-  { name: "Fresh Meat", href: "/shop/shop-3-column" },
-  { name: "Vegetables", href: "/shop/shop-3-column" },
-  { name: "Fruit & Nut Gifts", href: "/shop/shop-3-column" },
-  { name: "Fresh Berries", href: "/shop/shop-3-column" },
-  { name: "Ocean Foods", href: "/shop/shop-3-column" },
-  { name: "Butter & Eggs", href: "/shop/shop-3-column" },
-  { name: "Fastfood", href: "/shop/shop-3-column" },
-  { name: "Fresh Onion", href: "/shop/shop-3-column" },
-  { name: "Papayaya & Crisps", href: "/shop/shop-3-column" },
-  { name: "Oatmeal", href: "/shop/shop-3-column" },
-  { name: "Fresh Bananas", href: "/shop/shop-3-column" },
+  { name: "Fiction Books", href: "/shop/shop-3-column" },
+  { name: "Romance Books", href: "/shop/shop-3-column" },
+  { name: "Love Books", href: "/shop/shop-3-column" },
+  { name: "Life Books", href: "/shop/shop-3-column" },
+  { name: "Saving Books", href: "/shop/shop-3-column" },
+  { name: "Organic Foods", href: "/shop/shop-3-column" },
+  { name: "Work from Setup ", href: "/shop/shop-3-column" },
 ];
 
 function CategoryColappse({ active }) {
   const { Panel } = Collapse;
   return (
     <div className="category-collapse">
-      <Collapse
-        bordered={false}
-        defaultActiveKey={active ? "1" : null}
-        expandIcon={({ isActive }) => (
-          <DownOutlined rotate={isActive ? -180 : 0} />
-        )}
-        expandIconPosition="right"
-      >
-        <Panel
-          header="All departments"
-          key="1"
-          extra={<i className="far fa-bars" />}
-        >
+      <Collapse bordered={false} defaultActiveKey={active ? "1" : null} expandIcon={({ isActive }) => <DownOutlined rotate={isActive ? -180 : 0} />} expandIconPosition="right">
+        <Panel header="All departments" key="1" extra={<i className="far fa-bars" />}>
           <ul>
             {categories.map((item, index) => (
               <li key={index}>
