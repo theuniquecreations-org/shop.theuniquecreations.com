297a7aa006363818bed1945fae6adb9c53247943
bala <vbalakumar.cse@gmail.com>
2024-10-16 16:11:35 -0500

    fix category

--- src/components/AdminExpenseTracker/AdminExpenseTracker.js
+++ src/components/AdminExpenseTracker/AdminExpenseTracker.js
@@ -18,7 +18,7 @@ const ExpenseTracker = () => {
   };
 
   // Predefined categories
-  const categories = ["Groceries", "Subscription", "Rent", "Travel", "Shopping", "Entertainment", "Healthcare", "Other"];
+  const categories = ["Groceries", "Subscription", "Rent", "Travel", "Saving/Investment", "Shopping", "Entertainment", "Healthcare", "Other"];
   const [form, setForm] = useState({
     description: "",
     amount: "",
