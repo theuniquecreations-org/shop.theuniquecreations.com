8956db6655195d6b3d6a24abb146f860fd432257
bala <vbalakumar.cse@gmail.com>
2024-10-18 11:55:52 -0500

    fix app home icon

--- src/pages/adminapphome.js
+++ src/pages/adminapphome.js
@@ -1,26 +1,59 @@
-import PageBanner from "@/components/BannerSection/PageBanner";
-import AdminExpenseTracker from "@/components/AdminAppHome/AdminAppHome";
-import HeaderOne from "@/components/header/HeaderOne";
-import MobileMenu from "@/components/header/MobileMenu";
 import Layout from "@/components/Layout/Layout";
-import MainFooter from "@/components/MainFooter/MainFooter";
-import Style from "@/components/Reuseable/Style";
-import SearchPopup from "@/components/SearchPopup/SearchPopup";
 import React from "react";
-import image from "@/images/pr_source.png";
-import icon from "@/images/subaapplogo.jpg";
+import image from "@/images/pr_source.png"; // Main page image
+import icon from "@/images/subaapplogo.jpg"; // Icon image for the app
 
 const AdminHome = () => {
   return (
     <Layout pageTitle="Tales of SuBa - App Home" thumbnail={image.src} icon={icon.src} themecolor="#ffffff">
-      {/* <Style /> */}
-      {/* <HeaderOne /> */}
-      {/* <MobileMenu /> */}
-      {/* <SearchPopup /> */}
-      {/* <PageBanner title="Admin Expense Tracker" page="adminhome" parent="" parentHref="/" /> */}
-      {/* <MainFooter /> */}
+      <div className="container" style={styles.container}>
+        {/* iPhone-like app icons */}
+        <div style={styles.iconContainer}>
+          <a href="/adminexpenses" style={styles.appIconLink}>
+            <img src={icon.src} alt="Expense Tracker" style={styles.appIcon} />
+            <p style={styles.appText}>Expense Tracker</p>
+          </a>
+        </div>
+        <div style={styles.iconContainer}>
+          <a href="/adminsplitwise" style={styles.appIconLink}>
+            <img src={icon.src} alt="Split Equal" style={styles.appIcon} />
+            <p style={styles.appText}>Split Equal</p>
+          </a>
+        </div>
+      </div>
     </Layout>
   );
+};
+
+// CSS styles for iPhone-like app icons
+const styles = {
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    height: "100vh",
+    gap: "20px",
+  },
+  iconContainer: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  appIconLink: {
+    textDecoration: "none",
+    textAlign: "center",
+  },
+  appIcon: {
+    width: "100px",
+    height: "100px",
+    borderRadius: "20px",
+    boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.2)",
+  },
+  appText: {
+    marginTop: "8px",
+    fontSize: "14px",
+    color: "#000",
+  },
 };
 
 export default AdminHome;
